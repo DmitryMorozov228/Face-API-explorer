3243ebb9485ae0488b46dfcd5d83862333706b53
SOFTEQ/dmorozov <dmitriy.morozov@softeq.com>
2019-11-11 01:42:23 +0300

    Initial project.

---
+++ src/app/face-tester/face-tester.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FaceApiService } from '../services/face-api-service.service';
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'app-face-tester',
+  templateUrl: './face-tester.component.html',
+  styleUrls: ['./face-tester.component.css']
+})
+export class FaceTesterComponent implements OnInit {
+  loading = false;
+  public detectedFaces: any;
+  public identifiedPersons = [];
+  public imageUrl: string;
+  public multiplier: number;
+  public personGroups = [];
+  public selectedFace: any;
+  public selectedGroupId = '';
+  @ViewChild('mainImg') mainImg;
+
+  constructor(private faceApi: FaceApiService) { }
+
+  ngOnInit() {
+    this.loading = true;
+    this.faceApi.getPersonGroups().subscribe(data => {
+      this.personGroups = data;
+      this.loading = false;
+    });
+  }
+
+  detect() {
+    this.loading = true;
+    this.faceApi.detect(this.imageUrl).subscribe(data => {
+      this.detectedFaces = data;
+      console.log('**detect results', this.detectedFaces);
+      this.loading = false;
+    });
+
+  }
+
+  faceClicked(face) {
+    this.selectedFace = face;
+    if (this.selectedFace.identifiedPersonId) {
+      let identifiedPerson = _.find(this.identifiedPersons, { 'personId': face.identifiedPersonId });
+      this.selectedFace.name = identifiedPerson.name;
+    }
+  }
+
+  imageLoaded($event) {
+    this.selectedFace = null;
+    this.detectedFaces = [];
+    let img = this.mainImg.nativeElement;
+    this.multiplier = img.clientWidth / img.naturalWidth;
+  }
+}
